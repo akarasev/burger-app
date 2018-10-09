89b1f28c642cfde7977614fedcb2ecfd62ea6e54
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-09 13:55:22 +0700

    Added default message on empty ingredient list.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredient => {
             return [...Array(props.ingredients[ingredient])].map((_, i) => {
                 return <BurgerIngredient key={ingredient + i} type={ingredient}/>
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={styles.Burger}>
