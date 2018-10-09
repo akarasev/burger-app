03e04e794046c45e615d7568d94303e31181feb5
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-09 13:46:34 +0700

    rendering ingredients from state.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])].map((_, i) => {
+                return <BurgerIngredient key={ingredient + i} type={ingredient}/>
+            })
+        });
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type={"bread-top"}/>
-            <BurgerIngredient type={"cheese"}/>
-            <BurgerIngredient type={"meat"}/>
+            {transformedIngredients}
             <BurgerIngredient type={"bread-bottom"}/>
         </div>
     );
