e721ec04bd80730efeece11a3676eeee77afefd4
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-10 11:17:29 +0700

    Added toolbar component; fixed styling.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,11 @@ import React from 'react';
 import styles from './Layout.module.css';
 
 import Aux from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, BackDrop</div>
+        <Toolbar/>
         <main className={styles.Content}>
             {props.children}
         </main>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,7 +10,7 @@ const modal = (props) => (
         <div
             className={styles.Modal}
             style={{
-                transform: props.show ? 'translate(0)' : "translate(-100vh)",
+                transform: props.show ? 'translate(0)' : "translate(0, -100vh)",
                 opasity: props.show ? '1' : '0'
             }}>
             {props.children}
