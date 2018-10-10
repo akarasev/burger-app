0d3d2acbb713a14d298b459272ed1e309aa8aedf
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-10 07:56:14 +0700

    Implemented modal's backdrop.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 import styles from './Modal.module.css';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const modal = (props) => (
-    <div
-        className={styles.Modal}
-        style={{
-            transform: props.show ? 'translate(0)' : "translate(-100vh)",
-            opasity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
+        <div
+            className={styles.Modal}
+            style={{
+                transform: props.show ? 'translate(0)' : "translate(-100vh)",
+                opasity: props.show ? '1' : '0'
+            }}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default modal;
