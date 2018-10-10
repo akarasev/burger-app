044e305a9e00e8f855390493d3e26c20a7172877
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-10 13:27:43 +0700

    Improving performance by optimizing Modal rendering.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,28 +3,36 @@ import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(key => {
-            return (
-                <li key={key}>
-                    <span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}
-                </li>
-            )
-        });
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>A burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total:</strong> {props.total.toFixed(2)}</p>
-            <p>Continue to purchase?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
+class OrderSummary extends React.Component {
 
-export default orderSummary;
+    componentWillUpdate() {
+        console.log("[OrderSummary] will update");
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(key => {
+                return (
+                    <li key={key}>
+                        <span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}
+                    </li>
+                )
+            });
+        return (
+            <Aux>
+                <h3>Your order</h3>
+                <p>A burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total:</strong> {this.props.total.toFixed(2)}</p>
+                <p>Continue to purchase?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+
+}
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,18 +4,31 @@ import styles from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translate(0)' : "translate(0, -100vh)",
-                opasity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends React.Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, prevProps) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log("[Modal] will update");
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.show ? 'translate(0)' : "translate(0, -100vh)",
+                        opasity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
